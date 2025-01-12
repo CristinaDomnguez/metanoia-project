b957cf88958c8f4508f730f4bfaae97b40dc1cb8
José Miguel <Tapuy@hotmail.es>
2025-01-12 17:00:00 +0100

    dev: add navbar to resources page

--- frontend/src/pages/Resources.jsx
+++ frontend/src/pages/Resources.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { NavBar } from '../components/NavBar/NavBar'
 
 export default function Resources() {
     return (
         <>
+            <NavBar />
             <h2>Recursos</h2>
         </>
     )
