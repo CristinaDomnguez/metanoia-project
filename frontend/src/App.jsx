85c657f86d18b87111fc8fec262ee6eb614210f8
Cristina Domínguez <dominguezlanzacristina@gmail.com>
2025-01-13 23:44:55 +0100

    feat:move router to App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,37 @@ import { Home } from "./pages/Home/Home";
 import "./App.css";
 import { NavBar } from "./components/NavBar/NavBar";
 import { Footer } from "./components/Footer/Footer";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import Resources from "./pages/Resources/Resources.jsx";
+
+// Configuración del enrutador
+const router = createBrowserRouter([
+  {
+    element: (
+      <>
+        <NavBar />
+        <Outlet />
+        <Footer />
+      </>
+    ),
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/recursos",
+        element: <Resources />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
     /* Contenedor principal de la aplicación */
     <div className="app">
-      <NavBar />
-      <Home />
-      <Footer />
+      <RouterProvider router={router}>ooo</RouterProvider>
     </div>
   );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,24 +1,10 @@
 import ReactDOM from "react-dom/client"; // Asegúrate de importar ReactDOM
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { StrictMode } from "react";
 import App from "./App.jsx";
-import Resources from "./pages/Resources/Resources.jsx";
-
-// Configuración del enrutador
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/recursos",
-    element: <Resources />,
-  },
-]);
 
 // Renderizar el enrutador en la raíz
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>
 );
