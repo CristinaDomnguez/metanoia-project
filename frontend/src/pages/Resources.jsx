374f16b73f63ce992ba3994d678f4fe05a098ed7
José Miguel <Tapuy@hotmail.es>
2025-01-13 18:06:59 +0100

    dev: add more text to resources page

--- frontend/src/pages/Resources.jsx
+++ frontend/src/pages/Resources.jsx
@@ -18,10 +18,20 @@ export default function Resources() {
             <section className="intro-section layout-box">
                 <h2 className="title">Recursos</h2>
                 <div className="text-container">
-                    <p className="text">
-                        Tener la opinion de gente versada en la materia siempre es una buena guía. Hemos hecho esta seleccion podcast y videos que creemos
-                        te aportaran luz.
-                    </p>
+                    <div className="container">
+                        <p className="text">
+                            Tener la opinion de gente versada en la materia siempre es una buena guía. Hemos hecho esta seleccion podcast y videos que creemos
+                            te aportaran luz.
+                        </p>
+                        <p className="text">
+                            Con los numerosos recursos que te ofrecemos, queremos que tengas suficiente material para que puedas
+                            elegir lo que mejor se ajusta a tus necesidades, sin tener que sentirte agobiado por la necesidad de
+                            visualizar o escuchar todo el material que ponemos a tu disposición.
+                        </p>
+                        <p className="text">Queremos ayudarte a que consigas ver otro color a la vida y que puedas superar todos los obstáculos
+                            que se presenten en tu camino.
+                        </p>
+                    </div>
                     {/* iconos */}
                     <div className="icons">
                         <div>
