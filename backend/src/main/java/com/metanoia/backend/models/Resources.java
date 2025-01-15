49fdb6b516eebb87bc890de6b14ae3c364882e49
José Miguel <Tapuy@hotmail.es>
2025-01-15 13:26:00 +0100

    fix: delete unique=true in Resources entity

--- backend/src/main/java/com/metanoia/backend/models/Resources.java
+++ backend/src/main/java/com/metanoia/backend/models/Resources.java
@@ -33,7 +33,7 @@ public class Resources {
     @Column(name = "url", nullable = false, unique = true)
     // La columna "url" no puede ser nula y debe tener valores únicos en la tabla
     private String url;
-    @Column(name = "image_url", nullable = false, unique = true) // Columna para almacenar la URL de la imagen
+    @Column(name = "image_url", nullable = false) // Columna para almacenar la URL de la imagen
     private String imageUrl;
 
 }
