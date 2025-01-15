2ecc56e6e5c72db189fadb3496c1003624507278
José Miguel <Tapuy@hotmail.es>
2025-01-15 11:29:21 +0100

    fix: modify directorie and files structure resources page

---
+++ frontend/src/pages/Resources/Intro/Intro.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Intro.css";
+
+export default function Intro() {
+  return (
+    <>
+      {/* Sección introductoria */}
+      <section className="intro-section layout-box">
+        <div className="text-container">
+          <div className="container">
+            <p className="text">
+              Tener la opinion de gente versada en la materia siempre es una
+              buena guía. Hemos hecho esta seleccion podcast y videos que
+              creemos te aportaran luz.
+            </p>
+            <p className="text">
+              Con los numerosos recursos que te ofrecemos, queremos que tengas
+              suficiente material para que puedas elegir lo que mejor se ajusta
+              a tus necesidades, sin tener que sentirte agobiado por la
+              necesidad de visualizar o escuchar todo el material que ponemos a
+              tu disposición.
+            </p>
+            <p className="text">
+              Queremos ayudarte a que consigas ver otro color a la vida y que
+              puedas superar todos los obstáculos que se presenten en tu camino.
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
--- frontend/src/pages/Resources/Resources.jsx
+++ frontend/src/pages/Resources/Resources.jsx
@@ -1,32 +1,10 @@
-import "./Resources.css";
-import PodcastsAndVideos from "../../components/PodcastsAndVideos/PodcastsAndVideos";
+import PodcastsAndVideos from "./PodcastsAndVideos/PodcastsAndVideos";
+import Intro from "./Intro/Intro";
 
 export default function Resources() {
   return (
     <>
-      {/* Sección introductoria */}
-      <section className="intro-section layout-box">
-        <div className="text-container">
-          <div className="container">
-            <p className="text">
-              Tener la opinion de gente versada en la materia siempre es una
-              buena guía. Hemos hecho esta seleccion podcast y videos que
-              creemos te aportaran luz.
-            </p>
-            <p className="text">
-              Con los numerosos recursos que te ofrecemos, queremos que tengas
-              suficiente material para que puedas elegir lo que mejor se ajusta
-              a tus necesidades, sin tener que sentirte agobiado por la
-              necesidad de visualizar o escuchar todo el material que ponemos a
-              tu disposición.
-            </p>
-            <p className="text">
-              Queremos ayudarte a que consigas ver otro color a la vida y que
-              puedas superar todos los obstáculos que se presenten en tu camino.
-            </p>
-          </div>
-        </div>
-      </section>
+      <Intro />
       <PodcastsAndVideos />
     </>
   );
