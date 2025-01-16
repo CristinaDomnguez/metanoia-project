557a4eb1fefa64d54be6b922a6ed168127d6a282
José Miguel <Tapuy@hotmail.es>
2025-01-16 10:19:39 +0100

    fix: adjust maquetacion intro part in resources page

--- frontend/src/pages/Resources/Intro/Intro.jsx
+++ frontend/src/pages/Resources/Intro/Intro.jsx
@@ -7,24 +7,19 @@ export default function Intro() {
       {/* Sección introductoria */}
       <section className={`${styles.introSection} ${styles.layoutBox}`}>
         <div className={styles.textContainer}>
-          <div className={styles.container}>
-            <p className={styles.text}>
-              Tener la opinión de gente versada en la materia siempre es una
-              buena guía. Hemos hecho esta selección de podcasts y videos que
-              creemos te aportarán luz.
-            </p>
-            <p className={`${styles.text} ${styles.secondParagraph}`}>
-              Con los numerosos recursos que te ofrecemos, queremos que tengas
-              suficiente material para que puedas elegir lo que mejor se ajusta
-              a tus necesidades, sin tener que sentirte agobiado por la
-              necesidad de visualizar o escuchar todo el material que ponemos a
-              tu disposición.
-            </p>
-            <p className={styles.text}>
-              Queremos ayudarte a que consigas ver otro color a la vida y que
-              puedas superar todos los obstáculos que se presenten en tu camino.
-            </p>
-          </div>
+          <p className={`${styles.text} ${styles.secondText}`}>
+            Tómate tu tiempo y no te agobies. Visualiza la información
+            tranquilamente y comienza a inspirarte con las palabras de
+            excelentes profesionales.
+          </p>
+          <p className={`${styles.text} ${styles.secondParagraph}`}>
+            Buscamos tu <span className={styles.span}>bienestar</span>. Por eso
+            hemos reunido una selección de vídeos y podcasts creados por
+            expertos en salud mental.
+          </p>
+          <p className={`${styles.text} ${styles.thirdParagraph}`}>
+            ¡Únete a nuestra comunidad y encuentra el apoyo que necesitas!
+          </p>
         </div>
       </section>
     </>
