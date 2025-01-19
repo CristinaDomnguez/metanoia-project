fc2033810d496f566a3c0ad5cce90d40bcc7ed91
Cristina Domínguez <dominguezlanzacristina@gmail.com>
2025-01-19 20:35:51 +0100

    feat:modify intro resources

--- frontend/src/pages/Resources/Intro/Intro.jsx
+++ frontend/src/pages/Resources/Intro/Intro.jsx
@@ -1,33 +1,20 @@
 import styles from "./Intro.module.css"; // Importa el CSS Module
 
-
 export default function Intro() {
   return (
     <>
       {/* Sección introductoria */}
       <section className={styles.introSection}>
-        <div className={styles.textContainer}>
-        <div className={styles.container}>
-          <p className={styles.text}>
-            Tómate tu tiempo y no te agobies. Visualiza la información
-            tranquilamente y comienza a inspirarte con las palabras de
-            excelentes profesionales.
-          </p>
-          <p className={styles.text}>
-            Buscamos tu <span className={styles.span}>bienestar</span>. Por eso
-            hemos reunido una selección de vídeos y podcasts creados por
-            expertos en salud mental.
-          </p>
-          <p className={styles.text}>
-            ¡Únete a nuestra comunidad y encuentra el apoyo que necesitas!
-          </p>
-          </div>
-        {/* Ícono PNG añadido */}
+        <div className={styles.container}> 
         <img
-          src="/images/Resources/resourceGif.gif" 
-          alt="gif decorativo"
-          className={styles.icon}
+          src="/images/Resources/VídeosPodcast1.png"
+          className={styles.image}
         />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.textTop}> DESCUBRE NUEVAS FORMAS</h1>
+          <h2 className={styles.textBottom}> DE SENTIRTE BIEN</h2>
+          <div className={styles.container}></div>
         </div>
       </section>
     </>
