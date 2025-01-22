9458c53c128d4641675c35cb8ebda8b8f9caf33d
gnicolie <gnicolie@intra>
2025-01-22 13:10:45 +0100

    formulario

--- frontend/src/pages/Admin/Admin.jsx
+++ frontend/src/pages/Admin/Admin.jsx
@@ -1,7 +1,12 @@
+import React from 'react';
 import styles from "./Admin.module.css";
+import { Form } from './Form';
 
-export function Admin(){
-    return(
-        <div className={styles.main}>Hola Mundo</div>
-    );
+export function Admin() {
+  return (
+    <div className={styles.main}>
+      <h1>Admin Page</h1>
+      <Form />
+    </div>
+  );
 }
